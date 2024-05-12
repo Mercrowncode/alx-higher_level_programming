27112e37bcb44630f7aca1a51b502b9cd5e11480
Mercrowncode <mercy09203@gmail.com>
2024-05-12 22:30:52 +0100

    2-print, print and run!

---
+++ 2-print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# This is a python script that prints an array of strings
+# In this script, we will use the 'print()' function. 
+
+print('"Programming is like building a multilingual puzzle')
